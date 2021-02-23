1ae22a7d6815c3bb3f95d34a6c115d6df6f65fc0
Eric Born <eric.born85@gmail.com>
2021-02-23 17:46:16 -0600

    Impement delete text from window

--- UI.py
+++ UI.py
@@ -6,44 +6,39 @@ Created on Mon Feb 22 20:00:45 2021
 """
 
 import tkinter as tk
-# from backup.py import start_backup
+#from backup import start_backup
 
-# function to change the status text
-# def update_status_text():
-#     if activate_button.config('text')[-1] == 'Start Program':
-#         activate_button.config(text='Stop Program')
-#     else:
-#         activate_button.config(text='Start Program')
-
-def toggle():
+def toggle_program():
     '''
-    use
-    t_btn.config('text')[-1]
-    to get the present state of the toggle button
+    function to enable the backup and toggle button/status text
     '''
     if activate_button.config('text')[-1] == 'Start Program':
         activate_button.config(text='Stop Program')
-        status_text.delete('0.6')
+        status_text.delete(1.0, tk.END)
         status_text.insert(tk.END, 'Running')
+        return(True)
     else:
         activate_button.config(text='Start Program')
-        status_text.delete('0.6')
+        status_text.delete(1.0, tk.END)
         status_text.insert(tk.END, 'Stopped')
+        return(False)
 
 # create the window, text and button widgets
 window = tk.Tk()
 status_text = tk.Text(window)
 
+# activate button to start/stop the backups
 activate_button = tk.Button(window, text='Start Program',
-                            command = toggle)
+                            command = toggle_program)
 
+# close button to exit the program gracefully
 close_button = tk.Button(window, text = 'Exit', command = window.destroy)
 
 # status text
 running_text = 'Running'
 stopped_text = 'Stopped'
 
-# pack the widets
+# pack the text and button widets
 status_text.pack()  
 activate_button.pack()
 close_button.pack()
@@ -51,6 +46,5 @@ close_button.pack()
 # insert the text into the
 status_text.insert(tk.END, 'Stopped')
 
-#def backup_call_back():
-
+# call the tk function to draw the window
 window.mainloop()
