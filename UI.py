8a03d1fe21d1c2be1d363cb7144e01b5ada5dbbe
Eric Born <eric.born85@gmail.com>
2021-02-22 21:18:08 -0600

    Implement buttons and text widget

--- UI.py
+++ UI.py
@@ -6,30 +6,30 @@ Created on Mon Feb 22 20:00:45 2021
 """
 
 import tkinter as tk
-from backup.py import start_backup
+# from backup.py import start_backup
 
 # create the window, text and button widgets
 window = tk.Tk()
-#status_text = tk.Text(window)
+status_text = tk.Text(window)
 
 #activate_button_text = ['Start Program','Stop Program']
 
 activate_button = tk.Button(window, text='Start Program')
                             #command = start_backup)
 
-#close_button = tk.Button(window, text = 'Exit', command = window.destroy)
+close_button = tk.Button(window, text = 'Exit', command = window.destroy)
 
 # status text
 running_text = 'Running'
-stopped_text = 'Stopped'
+# stopped_text = 'Stopped'
 
 # pack the widets
-#status_text.pack()  
+status_text.pack()  
 activate_button.pack()
-#close_button.pack()
+close_button.pack()
 
 # insert the text into the
-#status_text.insert(tk.END, running_text)
+status_text.insert(tk.END, running_text)
 
 #def backup_call_back():
 
