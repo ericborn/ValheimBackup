bd2e25008aec2a2118e6fa96865c02563f66d5ff
Eric Born <eric.born85@gmail.com>
2021-02-23 19:17:05 -0600

    Adding start/stop and loop functionality

--- UI.py
+++ UI.py
@@ -6,22 +6,54 @@ Created on Mon Feb 22 20:00:45 2021
 """
 
 import tkinter as tk
-#from backup import start_backup
+# import os
+# import shutil
+#from time import sleep
+
+#import backup as bk
+
+running = True  # Global flag
+
+def scanning():
+    if running:  # Only do this if the Stop button has not been clicked
+        print('hello')
+
+    # After 1 second, call scanning again (create a recursive loop)
+    window.after(1000, scanning)
+    
+def start():
+    """Enable scanning by setting the global flag to True."""
+    global running
+    running = True
+
+def stop():
+    """Stop scanning by setting the global flag to False."""
+    global running
+    running = False
+
 
 def toggle_program():
     '''
     function to enable the backup and toggle button/status text
     '''
+    #main()
     if activate_button.config('text')[-1] == 'Start Program':
-        activate_button.config(text='Stop Program')
+        activate_button.config(text='Stop Program', command=start)
         status_text.delete(1.0, tk.END)
         status_text.insert(tk.END, 'Running')
-        return(True)
+        #print('started')
+        #window.after(10000, delayed)
+        print(True)
+        #return(True)
     else:
-        activate_button.config(text='Start Program')
+        activate_button.config(text='Start Program', command=stop)
         status_text.delete(1.0, tk.END)
         status_text.insert(tk.END, 'Stopped')
-        return(False)
+        #print('stopped')
+        #return(False)
+        print(False)
+
+
 
 # create the window, text and button widgets
 window = tk.Tk()
@@ -31,19 +63,21 @@ status_text = tk.Text(window)
 activate_button = tk.Button(window, text='Start Program',
                             command = toggle_program)
 
+# start button
+start_button = tk.Button(window, text='Start Program', command = start)
+
+# stop button
+stop_button = tk.Button(window, text='Start Program', command = stop)
+
 # close button to exit the program gracefully
 close_button = tk.Button(window, text = 'Exit', command = window.destroy)
-
-# status text
-running_text = 'Running'
-stopped_text = 'Stopped'
 
 # pack the text and button widets
 status_text.pack()  
 activate_button.pack()
 close_button.pack()
 
-# insert the text into the
+# insert the default text into the widget
 status_text.insert(tk.END, 'Stopped')
 
 # call the tk function to draw the window
